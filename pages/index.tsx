b549b155b029c2a02cd7d9fe0577ba9e1723a638
Yuuringo <yuto1birth11faml@gmail.com>
2021-12-01 17:27:07 +0900

    add header and sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import styles from '../css/styles.module.css'
+import Header from '../components/Header'
 
 const HomePage = () => {
-  return <div className={styles.hello}>Welcome to Next.js!</div>
+  return (
+    <>
+      <Header />
+      <div className={styles.hello}>Welcome to Next.js!</div>
+    </>
+  )
 }
 
 export default HomePage
